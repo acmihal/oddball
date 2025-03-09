bfe575b67bee5b81da806a2ce4da10ca1798f45d
Andrew Mihal <andrewcmihal@gmail.com>
2025-03-09 09:48:21 -0700

    Precalculated results

---
+++ make_results.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+set -x
+
+#for i in {3..4}
+#do
+#    oddball $i 2 | tee results/oddball_${i}_2.out
+#done
+#
+#for i in {3..13}
+#do
+#    oddball $i 3 | tee results/oddball_${i}_3.out
+#done
+
+for i in {3..80}
+do
+    oddball $i 4 | tee results/oddball_${i}_4.out
+done
