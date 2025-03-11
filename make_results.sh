9853fa06ca3be263ef8c31730ffb374b9ec1f97a
Andrew Mihal <andrewcmihal@gmail.com>
2025-03-11 14:58:05 -0700

    Refreshed pre-calculated results

--- make_results.sh
+++ make_results.sh
@@ -2,17 +2,17 @@
 
 set -x
 
-#for i in {3..4}
-#do
-#    oddball $i 2 | tee results/oddball_${i}_2.out
-#done
-#
-#for i in {3..13}
-#do
-#    oddball $i 3 | tee results/oddball_${i}_3.out
-#done
+for i in {3..4}
+do
+    oddball $i 2 | tee results/oddball_${i}_2.out
+done
 
-for i in {3..80}
+for i in {3..13}
+do
+    oddball $i 3 | tee results/oddball_${i}_3.out
+done
+
+for i in {3..40}
 do
     oddball $i 4 | tee results/oddball_${i}_4.out
 done
