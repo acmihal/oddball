fa71f0506f9dff949d76dc666944c72feae51449
Andrew Mihal <andrewcmihal@gmail.com>
2025-04-18 09:06:41 -0700

    Fix run_benchmarks for gzipped cnfs

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-ls benchmarks/*.cnf.gz | parallel timeout --signal INT 1h kissat {} ">" {.}.cert
+ls benchmarks/*.cnf.gz | parallel --plus timeout --signal INT 1h kissat {} ">" {..}.cert
+
+# Sort results by Kissat process-time.
+grep -H process-time benchmarks/*.cert | awk '{print $1, $(NF-1)}' | sort -n -k 2 > benchmarks/sorted_runtimes.txt
